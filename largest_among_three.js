9c473dd512a6d26f29d466cbd81b2f3a703b55db
OIENDRILA-P <160148965+OIENDRILA-P@users.noreply.github.com>
2025-09-19 17:03:54 +0530

    Create largest_among_three.js

---
+++ largest_among_three.js
@@ -0,0 +1,5 @@
+const firstNumber = 12;
+const secondNumber =17;
+const thirdNumber = 17;
+const result = (firstNumber>secondNumber)?(firstNumber>thirdNumber)?firstNumber:secondNumber:(secondNumber>thirdNumber)?secondNumber:thirdNumber;
+console.log("Largest number is", result);
