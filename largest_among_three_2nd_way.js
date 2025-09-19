2247e2534e6810bb0f67131cc7322ce034223aef
OIENDRILA-P <160148965+OIENDRILA-P@users.noreply.github.com>
2025-09-19 17:21:04 +0530

    Create largest_among_three_2nd_way.js

---
+++ largest_among_three_2nd_way.js
@@ -0,0 +1,14 @@
+const num1=5;
+const num2=35;
+const num3=25;
+let result;
+if(num1>num2&&num1>num2){
+   result = num1;
+}
+else if(num2>num1&&num2>num3){
+   result = num2;
+}
+else{
+   result = num3;
+}
+console.log("Largest number is:",result);
