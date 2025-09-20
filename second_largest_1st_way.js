9a966fca8e6cb53735751eaca362966218c6a217
OIENDRILA-P <160148965+OIENDRILA-P@users.noreply.github.com>
2025-09-20 11:07:03 +0530

    Update second_largest_1st_way.js

--- second_largest_1st_way.js
+++ second_largest_1st_way.js
@@ -1,5 +1,5 @@
 const number1 = 10;
 const number2 = 35;
 const number3 = 20;
-const result=(number1>number2&&number3>number2)?number1:(number2>number1&&number3>number2)?number2:(number3>number1&&number2>number3)?number3:number1;
-console.log("Second Largest Number is:",result);
+const result = (number1 > number2 && number3 > number2) ? number1 : (number2 > number1 && number3 > number2) ? number2 : (number3 > number1 && number2 > number3) ? number3 : number1;
+console.log("Second Largest Number is:", result);
