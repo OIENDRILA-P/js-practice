c738a6ea5455ed66fd4616cb5e9137b53ae981f7
OIENDRILA-P <160148965+OIENDRILA-P@users.noreply.github.com>
2025-09-20 11:05:48 +0530

    Update second_largest_number_2nd_way.js

--- second_largest_number_2nd_way.js
+++ second_largest_number_2nd_way.js
@@ -1,13 +1,14 @@
-const number1=15;
-const number2=10;
-const number3=25;
-if(((number1>number2)&&(number1<number3))||((number1<number2)&&(number1>number3))){
-    console.log("Second Largest number=",number1);
+const number1 = 15;
+const number2 = 10;
+const number3 = 25;
+if (((number1 > number2) && (number1 < number3)) || ((number1 < number2) && (number1 > number3))) {
+    console.log("Second Largest number=", number1);
 }
-else if(((number2>number1)&&(number2<number3))||((number2<number1)&&(number2>number3))){
-    console.log("Second Largest number=",number2);
+else if (((number2 > number1) && (number2 < number3)) || ((number2 < number1) && (number2 > number3))) {
+    console.log("Second Largest number=", number2);
 }
-else{
-    console.log("Second Largest number=",number3);
-    
+else {
+    console.log("Second Largest number=", number3);
+
 }
+
