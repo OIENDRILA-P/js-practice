160d2f53f29fb533c32146f02b6f4d5b10dbdd7b
OIENDRILA-P <160148965+OIENDRILA-P@users.noreply.github.com>
2025-09-20 11:24:59 +0530

    Updated spaces largest_among_three_2nd_way.js

--- largest_among_three_2nd_way.js
+++ largest_among_three_2nd_way.js
@@ -1,14 +1,14 @@
-const num1=5;
-const num2=35;
-const num3=25;
+const num1 = 5;
+const num2 = 35;
+const num3 = 25;
 let result;
-if(num1>num2&&num1>num2){
+if (num1 > num2 && num1 > num2) {
    result = num1;
 }
-else if(num2>num1&&num2>num3){
+else if (num2 > num1 && num2 > num3) {
    result = num2;
 }
-else{
+else {
    result = num3;
 }
-console.log("Largest number is:",result);
+console.log("Largest number is:", result);
