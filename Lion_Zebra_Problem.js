e83588e1577d2e8b39ca2551fc729c129d80ff61
OIENDRILA-P <160148965+OIENDRILA-P@users.noreply.github.com>
2025-09-21 12:06:24 +0530

    Update Lion_Zebra because the previous code was not running for some of the testcases

--- Lion_Zebra_Problem.js
+++ Lion_Zebra_Problem.js
@@ -1,4 +1,4 @@
-const testCase1 = "ZLL   ZZZL";
+const testCase1 = "L   Z     L Z";
 let lion1 = 0;
 let zebra1 = 0;
 let lCount = 0;
@@ -8,28 +8,23 @@ let lCount2 = testCase1.length;
 let zCount2 = 0;
 let outputCount1;
 let zebra2 = 0;
+let min = 99;
 let outputCount2;
 for (let count = 0; count < testCase1.length; count++) {
     if (testCase1[count] === "L") {
         lion1++;
         lCount = count;
     }
-    if (lion1 >= 1) {
+    else if (lion1 >= 1) {
         if (testCase1[count] === "Z") {
             zebra1++;
-        }
-        if (zebra1 === 1) {
             zCount = count;
             outputCount1 = (zCount - lCount) - 1;
-            if ((outputCount1 === 0)) {
-                break;
+            if (outputCount1 < min) {
+                min = outputCount1;
             }
         }
     }
-    else if (lCount === 0 || zCount === 0) {
-        outputCount1 = -1;
-    }
-
 }
 
 for (let count = testCase1.length - 1; count >= 0; count--) {
@@ -40,23 +35,18 @@ for (let count = testCase1.length - 1; count >= 0; count--) {
     if (lion2 >= 1) {
         if (testCase1[count] === "Z") {
             zebra2++;
-        }
-        if (zebra2 === 1) {
             zCount2 = count;
             outputCount2 = (lCount2 - zCount2) - 1;
-            if (outputCount2 === 0 && outputCount2 !== -1) {
-                console.log("outputCount2", outputCount2);
-                break;
+            if (outputCount2 < min) {
+                min = outputCount2;
             }
         }
     }
-    else if (lCount2 === 0 || zCount2 === 0) {
-        outputCount2 = -1;
-    }
 }
-if ((outputCount1 < outputCount2) && (outputCount1 != -1) || (outputCount1 > outputCount2 && outputCount2 != 0)) {
-    console.log("Input: ", testCase1, "Output: ", outputCount1);
+if(min!=99){
+    console.log("Input:", testCase1, "output:", min);
 }
-else {
-    console.log("Input: ", testCase1, "Output: ", outputCount2);
+else{
+    console.log("Input:", testCase1, "output:", -1);
+    
 }
