4728e992016df33a478a9f2f5e03d104750a4be6
OIENDRILA-P <160148965+OIENDRILA-P@users.noreply.github.com>
2025-10-12 20:05:54 +0530

    Update CowsAndBullsGame.js

--- CowsAndBullsGame.js
+++ CowsAndBullsGame.js
@@ -1,55 +1,93 @@
-function confirmation() {
-  const answer = confirm("Do you want to Play Again?");
-
-  if (answer) {
+function startAgain(yes) {
+  if (yes) {
     return playGame();
   }
 }
 
+function confirmation() {
+  return confirm("Do you want to Play Again?");
+}
+function displayLoseResult(secretNumber) {
+  console.log("YOU LOSE!!\nTHE SECRET NUMBER WAS :" + secretNumber.join(""));
+}
 
-function askingGuesses() {
-  const guess = prompt("Enter your guess");
+function displayWinResult() {
+  console.log("YOU WIN!!");
+}
 
-  return guess;
+function resultEqualityCheck(result, actualResult) {
+  for (let index = 0; index < actualResult.length; index++) {
+    if (result[index] !== actualResult[index]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function isEqual(secretNumber, guessMade, index) {
   return secretNumber[index] === guessMade[index] ? "Cow" : "Bull";
 }
 
-function matchTwoStrings(secretNumber, guessMade) {
-  const result = [];
+function guessCheckWithSN(guessMade, secretNumber) {
+  let result = [];
 
-  for (let index = 0; index < secretNumber.length; index = index + 2) {
-    result.push(isEqual(secretNumber, guessMade, index));
+  for (let i = 0; i < secretNumber.length; i++) {
+    result.push(isEqual(secretNumber, guessMade, i));
   }
-
-  return result.toString();
+  return result;
 }
 
+function convertToNumberArray(array) {
+  let numberArray = [];
+  for (let index = 0; index < array.length; index++) {
+    numberArray.push(parseInt(array[index]));
+  }
+  return numberArray;
+}
+
+function userGuess() {
+  let guess = [];
+  let numberArray = [];
+  guess = prompt("Enter your guess").split("");
+  numberArray = convertToNumberArray(guess);
+  return numberArray;
+}
+
+function instructions() {
+  console.log("This is a different version of COWS AND BULLS.\n");
+  console.log("COW WILL BE DECLARED: WHEN THE DIGIT IS CORRECT AND IN CORRECT LOCATION.\n");
+  console.log("BULL WILL BE DECLARED: WHEN THE DIGIT IS WRONG OR CORRECT IN WRONG LOCATION.\n");
+  console.log("Make a guess between 0-999");
+}
 
 function playGame() {
-  console.log("THIS COWS AND BULLS GAME IS MADE BY ME AND EXPLAINED BY AKKA🤪")
-  console.log("Make a guess between 0-999");
-  let secretNumber = Math.floor(Math.random() * 999).toString();
-  secretNumber = secretNumber.split("").toString();
+  instructions();
+  let secretNumber = Math.floor(Math.random() * 999).toString().padStart(3, "0").split("");
+  let numberArray = [];
+  let result = [];
+  const actualResult = [];
+  let confirmationAnswer = false;
+
+  numberArray = convertToNumberArray(secretNumber);
+
+  for (let i = 0; i < numberArray.length; i++) {
+    actualResult.push("Cow");
+  }
 
   for (let i = 0; i < 10; i++) {
-    let guessMade = askingGuesses().split("").toString();
-    let result = matchTwoStrings(secretNumber, guessMade);
+    result = guessCheckWithSN(userGuess(), numberArray);
+    console.log(result);
 
-    console.log(result + "\n");
-
-    if (result === "Cow, Cow, Cow") {
-      return console.log("YOU WON !!") + confirmation();
-    }
-    if (result === "Cow, Cow") {
-      return console.log("YOU WON !!") + confirmation();
+    if (resultEqualityCheck(result, actualResult)) {
+      displayWinResult();
+      confirmationAnswer = confirmation();
+      return startAgain(confirmationAnswer);
     }
 
   }
-  return console.log("YOU LOOSE!!\nTHE SECRET NUMBER WAS :" + secretNumber)
-    + confirmation();
+  displayLoseResult(secretNumber);
+  confirmationAnswer = confirmation();
+  return startAgain(confirmationAnswer);
 }
 
 playGame();
