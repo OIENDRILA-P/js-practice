084a6401775a68765eccae977c0829828810a9f0
OIENDRILA-P <160148965+OIENDRILA-P@users.noreply.github.com>
2025-10-28 17:20:59 +0530

    Update bencode.js

--- bencode.js
+++ bencode.js
@@ -8,30 +8,54 @@ function isNumber(value) {
 function encodeNumber(value) {
   return `i${value}e`;
 }
-function isStringOrList(value, information) {
-  return value === '[' ? encodeList(information) : encodeString(information);
+function isStringOrList(information) {
+  return information.startsWith('[') && information.endsWith(']') ? encodeList(information) :
+    encodeString(information);
 }
 function encodeString(value) {
   return `${value.length}:${value}`;
 }
-function spliceList(value) {
-  let a = value.split("");
-  a.splice(1, 0, " ");
-  a.splice(a.length - 1, 0, " ");
-  return a.join("");
+function encodeList(value) {
+  let list = spliceApplied(value).split(",");
+  for (let i = 0; i < list.length; i++) {
+    if (isNumber(parseInt(list[i]))) {
+      list[i] = encodeNumber(list[i]);
+    }
+    else if (list[i] === "[") {
+      list[i] = "l";
+    }
+    else if (list[i] === "]") {
+      list[i] = "e";
+    }
+    else if (!isNumber(parseInt(list[i]))) {
+      list[i] = encodeString(list[i]);
+    }
+  }
+  return list.join("");
 }
 function spliceApplied(value) {
-  const isList = value.startsWith('[') && value.endsWith(']');
-  return (isList && `[ ${spliceApplied(value.slice(1, -1))} ]`) || spliceList(value);
+  let splicedValue = "";
+  for (let i = 0; i < value.length - 1; i++) {
+    if (value[i] === "[") {
+      splicedValue += value[i] + ",";
+    }
+    else if (value[i] === "]") {
+      splicedValue += "," + value[i];
+    }
+    else {
+      splicedValue += value[i];
+    }
+  }
+  return splicedValue + ",]";
 }
 
 function encode() {
   const information = userInfo();
   for (let i = 0; i < information.length; i++) {
     if (!isNumber(parseInt(information[i]))) {
-      return isStringOrList(information[i], information);
+      return isStringOrList(information);
     }
   }
   return encodeNumber(information)
 }
-console.log(encode());
+encode();
