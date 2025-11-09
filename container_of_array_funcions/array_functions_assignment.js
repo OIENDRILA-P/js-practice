b01d490395752851cef0c545d14f2ac6ded78a77
OIENDRILA-P <160148965+OIENDRILA-P@users.noreply.github.com>
2025-11-09 11:39:25 +0530

    Implement decision checking and test functions
    Added functions to check decisions and display results.

--- container_of_array_funcions/array_functions_assignment.js
+++ container_of_array_funcions/array_functions_assignment.js
@@ -1,3 +1,4 @@
+
 const count = function (initialValue, element) {
   if (element === "blue") {
     initialValue++;
@@ -6,5 +7,32 @@ const count = function (initialValue, element) {
   return initialValue;
 };
 
-console.log(["red", "blue", "red", "green", "red", "blue"].reduce(count, 0));
+function checkDecision(message, actual, expected) {
+  return actual === expected ? passedMessage(message) :
+    failedMessage(message, actual, expected);
+}
 
+function failedMessage(message, actual, expected) {
+  return console.log("❌" + message, actual, expected);
+}
+
+function passedMessage(message) {
+  return console.log("✅" + message);
+}
+
+function testQuestion1(description, actual, expected) {
+  const result = checkDecision(description, actual, expected);
+  return result;
+}
+
+const testFestivalRibbonCount = function () {
+  console.log("The blue ribbons were cut are: ");
+  console.log("-".repeat(15));
+  testQuestion1("2 Blue Ribbons were cut", ["red", "blue", "red", "green", "red", "blue"].reduce(count, 0), 2);
+};
+
+const main = function () {
+  testFestivalRibbonCount();
+};
+
+main();
